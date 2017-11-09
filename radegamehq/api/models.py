e03a84bc961e6632e4fe88372ed06541e06b9dda
Zdravko Kirilov <zkirilov@kpmg.com>
2017-11-09 17:17:30 +0200

    latst changes

--- radegamehq/api/models.py
+++ radegamehq/api/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Game(models.Model):
+    TYPE_TERRITORY_MAP = 'TERRITORY_MAP'
+    TYPE_BASIC_GRID = 'BASIC_GRID'
+
+    TYPE_CHOICES = (
+        (TYPE_TERRITORY_MAP, 'TERRITORY_MAP'),
+        (TYPE_BASIC_GRID, 'BASIC_GRID'),
+    )
+
     title = models.CharField(max_length=255, blank=False, unique=False)
-    boardType = models.CharField(max_length=255, blank=False, unique=False)
+    boardType = models.CharField(max_length=255, blank=False, unique=False, choices=TYPE_CHOICES)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
