446aae55266be9a1e7f90848e4f1fbcea9286c06
Kirilov <zkirilov@kpmg.com>
2018-01-10 17:20:54 +0200

    latest activity changes

--- radegamehq/api/serializers.py
+++ radegamehq/api/serializers.py
@@ -34,55 +34,40 @@ class ActionSerializer(serializers.ModelSerializer):
 
     @transaction.atomic
     def create(self, validated_data):
-        income = validated_data.pop('income')
-        cost = validated_data.pop('cost')
-        field = BoardField.objects.create(name=validated_data['name'], description=validated_data['description'],
-                                          image=validated_data['image'], game=validated_data['game'])
+        configs = validated_data.pop('configs')
 
-        for item in income:
-            resource = Resource.objects.get(pk=item['resource'])
-            FieldIncome.objects.create(field=field, resource=resource, quantity=item['quantity'])
+        action = Action.objects.create(name=validated_data['name'], description=validated_data['description'],
+                                       image=validated_data['image'], game=validated_data['game'])
 
-        for item in cost:
-            resource = Resource.objects.get(pk=item['resource'])
-            FieldCost.objects.create(field=field, resource=resource, quantity=item['quantity'])
+        for item in configs:
+            ActionConfig.objects.create(action=action, mode=item['mode'], target=item['target'], type=item['type'],
+                                        bonus=item['bonus'])
 
-        return field
+        return action
 
     @transaction.atomic
     def update(self, instance, validated_data):
-        existing_income = FieldIncome.objects.filter(field=instance)
-        income = validated_data.pop('income')
-        income_ids = [item['id'] for item in income if 'id' in item]
-
-        existing_cost = FieldCost.objects.filter(field=instance)
-        cost = validated_data.pop('cost')
-        cost_ids = [item['id'] for item in cost if 'id' in item]
+        existing_configs = ActionConfig.objects.filter(action=instance)
+        configs = validated_data.pop('configs')
+        config_ids = [item['id'] for item in configs if 'id' in item]
 
         try:
-            existing_income.exclude(pk__in=income_ids).delete()
-            existing_cost.exclude(pk__in=cost_ids).delete()
-        except (FieldIncome.DoesNotExist, FieldCost.DoesNotExist) as e:
+            existing_configs.exclude(pk__in=config_ids).delete()
+        except ActionConfig.DoesNotExist:
             pass
 
-        for item in income:
-            resource = Resource.objects.get(pk=item['resource'])
+        for item in configs:
             try:
-                obj = FieldIncome.objects.get(pk=item['id'])
+                obj = ActionConfig.objects.get(pk=item['id'])
             except KeyError:
-                FieldIncome.objects.create(field=instance, resource=resource, quantity=item['quantity'])
+                ActionConfig.objects.create(action=instance, mode=item['mode'], target=item['target'],
+                                            type=item['type'],
+                                            bonus=item['bonus'])
             else:
-                obj.quantity = item['quantity']
-                obj.save()
-
-        for item in cost:
-            resource = Resource.objects.get(pk=item['resource'])
-            try:
-                obj = FieldCost.objects.get(pk=item['id'])
-            except KeyError:
-                FieldCost.objects.create(field=instance, resource=resource, quantity=item['quantity'])
-            else:
-                obj.quantity = item['quantity']
+                obj.bonus = item['bonus']
+                obj.type = item['type']
+                obj.mode = item['mode']
+                obj.target = item['target']
                 obj.save()
 
         instance.__dict__.update(**validated_data)
