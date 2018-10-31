484f15961115e2651e43c6b1f93578dcda93bfef
Zdravko <zdravkokirilov@yahoo.com>
2018-10-31 17:16:28 +0200

    Team entity

---
+++ radegamehq/api/entities/Team.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from .Pool import Pool
+
+from api.mixins.EntityBase import EntityBase
+
+
+class Team(EntityBase):
+    image = models.FileField(upload_to='team_images', blank=True, null=True, max_length=None)
+
+    min_players = models.IntegerField(default=1)
+    max_players = models.IntegerField(default=1)
+
+    effect_pool = models.ManyToManyField(Pool, related_name='faction_effect_pool', blank=True)
+
+    def __str__(self):
+        return self.name
