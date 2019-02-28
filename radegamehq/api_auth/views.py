b12a7414a5eb8ac0176bbd37d0fbec51c78c8898
zdravko.kirilov <zdravkovasilchevkirilov@gmail.com>
2019-02-28 10:07:07 +0200

    user auth changes

--- radegamehq/api_auth/views.py
+++ radegamehq/api_auth/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import status, permissions, decorators
 
 from .models import AppUser
 from .serializers import AppUserSerializer
@@ -9,7 +9,7 @@ from .token import create_token
 
 class GetCurrentUserView(RetrieveAPIView):
     serializer_class = AppUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
     def retrieve(self, request, *args, **kwargs):
         instance = request.user
@@ -30,14 +30,18 @@ class CreateLocalUserView(CreateAPIView):
         except AppUser.DoesNotExist:
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+            AppUser.objects.create(email=serializer.data['email'], password=serializer.initial_data['password'])
+            # self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class GetTokenView(CreateAPIView):
     serializer_class = AppUserSerializer
+    permission_classes = []
 
+    @decorators.authentication_classes([])
+    @decorators.permission_classes([])
     def create(self, request, *args, **kwargs):
         if 'email' not in request.data or 'password' not in request.data:
             return Response('email and password are required fields', status=status.HTTP_400_BAD_REQUEST)
