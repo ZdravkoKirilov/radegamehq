e891415aa066245713b9b7e0034296d33ac541ce
Zdravko Kirilov <Zdravko Kirilov@dxc.com>
2019-08-16 14:15:52 +0300

    entity changes

--- radegamehq/api/entities/Stage.py
+++ radegamehq/api/entities/Stage.py
@@ -8,5 +8,7 @@ class Stage(EntityBase, models.Model):
     width = models.IntegerField()
     height = models.IntegerField()
 
+    populate_by = models.ForeignKey('Expression', on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return "{}".format(self.name)
