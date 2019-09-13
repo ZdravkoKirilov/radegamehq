d00e0ad473e825099c888a0314587e4a67d77bdd
Zdravko Kirilov <Zdravko Kirilov@dxc.com>
2019-09-13 17:36:03 +0300

    Fetch single game instance

--- radegamehq/arena/views.py
+++ radegamehq/arena/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics
-
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import GameInstance
 from .serializers import GameInstanceSerializer
 
@@ -8,7 +8,10 @@ class GameInstanceView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = GameInstanceSerializer
 
     def get_queryset(self):
-        return GameInstance.objects.all().filter(public_id=self.kwargs['public_id'])
+        try:
+            return GameInstance.objects.get(public_id=self.kwargs['public_id'])
+        except GameInstance.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class ActiveGamesView(generics.ListAPIView):
