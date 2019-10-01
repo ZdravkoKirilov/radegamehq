d80bb19f689314cde05c0a5dc6a954dfa5fe0b6c
Zdravko Kirilov <Zdravko Kirilov@dxc.com>
2019-10-01 17:21:48 +0300

    transition enabling/disabling

--- radegamehq/api/entities/Transition.py
+++ radegamehq/api/entities/Transition.py
@@ -10,8 +10,11 @@ class Transition(models.Model):
     trigger = models.TextField()
     prop = models.CharField(max_length=255)
 
-    animation = models.ForeignKey('Animation', on_delete=models.CASCADE, related_name='animation', blank=True, null=True)
-    sound = models.ForeignKey('Sound', on_delete=models.CASCADE, related_name='sound', blank=True, null=True)
+    animation = models.ForeignKey('Animation', on_delete=models.SET_NULL, related_name='animation', blank=True,
+                                  null=True)
+    sound = models.ForeignKey('Sound', on_delete=models.SET_NULL, related_name='sound', blank=True, null=True)
+
+    enabled = models.ForeignKey('Expression', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return "{}".format(self.name)
