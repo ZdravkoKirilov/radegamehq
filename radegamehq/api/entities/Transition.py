5f444e32329d3ac3dc86b5b0637cbeffeb484f85
Zdravko Kirilov <zdravkokirilov@yahoo.com>
2019-12-09 10:06:35 +0200

    Sound updates

--- radegamehq/api/entities/Transition.py
+++ radegamehq/api/entities/Transition.py
@@ -11,7 +11,7 @@ class Transition(models.Model):
 
     animation = models.ForeignKey('Animation', on_delete=models.SET_NULL, related_name='animation', blank=True,
                                   null=True)
-    sound = models.ForeignKey('Sound', on_delete=models.SET_NULL, related_name='sound', blank=True, null=True)
+    sound = models.TextField(null=True, blank=True)
 
     enabled = models.TextField(blank=True, null=True)
 
